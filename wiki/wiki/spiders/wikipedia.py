78fb477b47b094e67bc234ff33dcf1261229b919
Святослав <swja7ik@mail.ru>
2024-01-31 22:03:34 +0300

    Innit

--- wiki/wiki/spiders/wikipedia.py
+++ wiki/wiki/spiders/wikipedia.py
@@ -7,7 +7,7 @@ class WikipediaSpider(Spider):
     start_urls = ["https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F%3A%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83"]
 
     def parse(self, response):
-        all_pages = response.xpath("//*[@class='external text']/@href").getall()[1:2]
+        all_pages = response.xpath("//*[@class='external text']/@href").getall()[1:]
         for url in all_pages:
             yield Request(
                 response.urljoin(url),
